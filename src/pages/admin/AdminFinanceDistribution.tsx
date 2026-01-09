a5a32afedabaed7d2e86d7fc50bd8db03edb9672
sabbaghh915 <sabbaghh915@gmail.com>
2026-01-09 06:15:23 +0300

    update Selecte

--- src/pages/admin/AdminFinanceDistribution.tsx
+++ src/pages/admin/AdminFinanceDistribution.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "../../components/ui/ca
 import { Button } from "../../components/ui/button";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../../components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../../components/ui/Select";
 import { Alert, AlertDescription } from "../../components/ui/alert";
 import { Separator } from "../../components/ui/separator";
 import { Badge } from "../../components/ui/badge";
